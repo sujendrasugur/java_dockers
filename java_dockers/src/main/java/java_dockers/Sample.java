ef94235c2fa819b47c0fe0cf50ed66ff1d00f291
sujendrasugur <sujendra.sugur@gmail.com>
2017-05-17 14:27:02 -0500

    next push

--- java_dockers/src/main/java/java_dockers/Sample.java
+++ java_dockers/src/main/java/java_dockers/Sample.java
@@ -3,7 +3,9 @@ package java_dockers;
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("HELLO FROM SUJJU... WELCOME TO DOCKERS DEMO....");
 
 	}
 
